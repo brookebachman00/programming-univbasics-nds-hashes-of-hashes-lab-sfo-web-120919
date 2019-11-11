bb06c626442bedf6442b1ca8fc79f016a15a1474
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 21:25:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,34 +32,28 @@
 #   label: "Species",
 #   sub_category: nil
 # }
-
 def naming_system
-  {
-  :PORTION_1 => {
-  :label => "Kingdom",
+{
   :sub_category => {
-  :label => "Phylum",
-  :sub_category => {
-  :label => "Class"
-  }
-   }
+    :label => "Kingdom",
+    :sub_category => {
+      :label => "Phylum",
+      :sub_category => {
+        :label => "Order"
+        :sub_category => {
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus"
+            :sub_category => {
+              :label => "Species",
+              :sub_category => nil
+            },
+          },
+        },
+      },
     },
+  },
 
- :PORTION_2 => {
- :label => "Order"
-},
-
-:PORTION_3 => {
-  :label => "Family",
-  :sub_category => {
-    :label => "Genus"
- }
-},
-
-:PORTION_4 => {
-:label => "Species",
-:sub_category => nil
-}
 }
   # Remember:
   #  Kingdom

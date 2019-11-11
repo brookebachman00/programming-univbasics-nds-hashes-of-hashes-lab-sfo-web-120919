460d40c2f1244afd7386f54a383464e73aae552e
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 21:08:41 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -8,27 +8,24 @@ def Hoh
   :label => "Class"
   }
    }
-    }
-  }
+    },
 
  :PORTION_2 => {
  :label => "Order"
-}
-
+},
 
 :PORTION_3 => {
   :label => "Family",
   :sub_category => {
     :label => "Genus"
  }
- }
-
+},
 
 :PORTION_4 => {
 :label => "Species",
 :sub_category => nil
 }
-
+}
 end
 
 
